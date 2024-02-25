db53bf9aaffcdfd7e107630ba9026576cbc077de
RO03M <romera.gv.s@gmail.com>
2024-02-24 22:33:46 -0300

    fix(lerp): added happy dom for testing and added color check in lerp method

--- src/math/lerp.ts
+++ src/math/lerp.ts
@@ -1,29 +1,24 @@
 import { scale } from "chroma-js";
 import { clamp } from "./clamp";
 
-export function lerp(from: string, to: string, time: number): string;
-export function lerp(from: number, to: number, time: number): number;
-export function lerp(from: unknown, to: unknown, time: number) {
+export function lerp(from: string | number, to: string | number, time: number) {
     if (typeof from === "number" && typeof to === "number") {
         time = clamp(time, 0, 1);
     
         return (1 - time) * from + time * to;
     } else if (typeof from === "string" && typeof to === "string") {
-        console.log(from);
-        // if (CSS.supports("color", from) && CSS.supports("color", to)) {
+        if (CSS.supports("color", from) && CSS.supports("color", to)) {
             return scale([from, to])(time).hex();
-        // } else {
-        //     throw new Error("Invalid color");
-        // }
+        } else {
+            throw new Error("Invalid color");
+        }
     } else {
         console.error(from, to);
         throw new Error("Invalid types for interpolation: expected string or number");
     }
 }
 
-export function lerpOverTime(from: string, to: string, duration: number, currentDuration: number): string;
-export function lerpOverTime(from: number, to: number, duration: number, currentDuration: number): number;
-export function lerpOverTime(from: unknown, to: unknown, duration: number, currentDuration: number) {
+export function lerpOverTime(from: string | number, to: string | number, duration: number, currentDuration: number) {
     const normalizedTime = currentDuration / duration;
     
     return lerp(from, to, normalizedTime);
